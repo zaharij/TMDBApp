979542df8033336db534cadeddea8e7a7115a036
Zakhar Yastrub <zahar.yastrub@gmail.com>
2017-10-27 16:42:25 +0300

    - last library dependencies added - renamed some fields and classes - fixed some architecture bugs (removed cash from data) - removed guava library + developed own FragmentSaver - loading image url dynamically - warnings fixed

--- app/src/main/java/com/centaurs/tmdbapp/util/NetworkConnection.java
+++ app/src/main/java/com/centaurs/tmdbapp/util/NetworkConnection.java
@@ -9,7 +9,7 @@ public class NetworkConnection {
     public static boolean isNetworkConnected(Context context){
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        NetworkInfo activeNetworkInfo = connectivityManager != null ? connectivityManager.getActiveNetworkInfo() : null;
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
